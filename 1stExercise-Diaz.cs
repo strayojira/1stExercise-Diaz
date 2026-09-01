64ebf0fe85d0a4bdcd6eebb41199281aaf647f88
ojira <diaz.jairomassimo@gmail.com>
2026-09-01 14:24:10 +0800

    Add student profile and menu display in C#

---
+++ 1stExercise-Diaz.cs
@@ -0,0 +1,53 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine ("Hello, C#!");
+        Console.WriteLine ("Welcome to Data Structures and Algorithms");
+        Console.WriteLine(); 
+
+        Console.WriteLine("=================================="); 
+        Console.WriteLine("          STUDENT PROFILE"); 
+        Console.WriteLine("==================================");
+        Console.WriteLine();
+        Console.WriteLine ("Name: Jairo Massimo Diaz");
+        Console.WriteLine ("Age: 18");
+        Console.WriteLine ("Course: BS Infomation Systems");
+        Console.WriteLine ("Year Level: Sophomore");
+        Console.WriteLine ("Section: BSIS02");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.WriteLine("=================================="); 
+        Console.WriteLine("          MENU"); 
+        Console.WriteLine("==================================");
+        Console.WriteLine();
+        Console.WriteLine ("1. Jolli Hotdog            PHP 67");
+        Console.WriteLine ("2. Burger Steak            PHP 123");
+        Console.WriteLine ("3. Apple Pay               PHP 88");
+        Console.WriteLine ("4. Candy                   PHP 2");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.WriteLine("********************************"); 
+        Console.WriteLine("      JAIRO MASSIMO DIAZ"); 
+        Console.WriteLine("          Insert Role"); 
+        Console.WriteLine("********************************");
+        Console.WriteLine();
+        Console.WriteLine ("Email:         diaz.jairomassimo@gmail.com");
+        Console.WriteLine ("Phone:         +63 967 676 6767");
+        Console.WriteLine ("Company:       Roblox Corporation");
+        Console.WriteLine ("Location:      Biringan City");
+        Console.WriteLine();
+        Console.WriteLine("********************************");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+    }
+}
